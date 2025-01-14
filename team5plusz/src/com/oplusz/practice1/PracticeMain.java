1ca483909ae2a1c5ac99aacc1c37019bfe543d24
hs-sg <hasin927@gmail.com>
2025-01-14 16:51:39 +0900

    마스터 보호 설정 확인용 커밋

--- team5plusz/src/com/oplusz/practice1/PracticeMain.java
+++ team5plusz/src/com/oplusz/practice1/PracticeMain.java
@@ -5,6 +5,7 @@ public class PracticeMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("리파지토리 연결 테스트");
+		System.out.println("마스터로 커밋");
 	}
 
 }
