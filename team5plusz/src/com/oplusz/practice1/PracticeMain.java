20ecb0a3845b86a3523f558343a76344a1e34ea9
hs-sg <hasin927@gmail.com>
2025-01-14 16:55:42 +0900

    커밋 test

--- team5plusz/src/com/oplusz/practice1/PracticeMain.java
+++ team5plusz/src/com/oplusz/practice1/PracticeMain.java
@@ -6,6 +6,7 @@ public class PracticeMain {
 		// TODO Auto-generated method stub
 		System.out.println("리파지토리 연결 테스트");
 		System.out.println("마스터로 커밋");
+		System.out.println("수정하려면 develop 브랜치를 사용하세요.");
 	}
 
 }
