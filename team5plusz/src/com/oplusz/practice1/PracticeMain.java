03886b8a4708eff9090ea9db40d5a6948b150227
hs-sg <hasin927@gmail.com>
2025-01-14 16:47:35 +0900

    커밋 테스트

---
+++ team5plusz/src/com/oplusz/practice1/PracticeMain.java
@@ -0,0 +1,10 @@
+package com.oplusz.practice1;
+
+public class PracticeMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("리파지토리 연결 테스트");
+	}
+
+}
