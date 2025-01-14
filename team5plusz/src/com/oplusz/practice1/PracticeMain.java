467301d7c69b72a0ea09ec1a69be9377b3047f26
hs-sg <hasin927@gmail.com>
2025-01-14 17:13:56 +0900

    test branch commit

--- team5plusz/src/com/oplusz/practice1/PracticeMain.java
+++ team5plusz/src/com/oplusz/practice1/PracticeMain.java
@@ -6,7 +6,7 @@ public class PracticeMain {
 		// TODO Auto-generated method stub
 		System.out.println("리파지토리 연결 테스트");
 		System.out.println("마스터로 커밋");
-		System.out.println("수정하려면 develop 브랜치를 사용하세요.");
+		System.out.println("새로 만든 브랜치로 커밋.");
 	}
 
 }
