ad11eac4ec969d14522b6b4825f11e98a918873d
hs-sg <hasin927@gmail.com>
2025-01-15 22:43:38 +0900

    노트북에서 커밋

--- team5plusz/src/com/oplusz/practice1/PracticeMain.java
+++ team5plusz/src/com/oplusz/practice1/PracticeMain.java
@@ -9,6 +9,7 @@ public class PracticeMain {
 		System.out.println("새로 만든 브랜치로 커밋.");
 		System.out.println("GitHub 에서 수정");
 		System.out.println("testBranch2에서 수정");
+		// 250115 노트북에서 다시 수정
 	}
 
 }
