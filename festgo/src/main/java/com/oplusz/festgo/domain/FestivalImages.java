ee4b06579ab5fba5fbc2debfc0835cbc71a27d9d
JONGHUKIM <hotsky803@gmail.com>
2025-01-22 11:28:31 +0900

    basic Tools

---
+++ festgo/src/main/java/com/oplusz/festgo/domain/FestivalImages.java
@@ -0,0 +1,16 @@
+package com.oplusz.festgo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class FestivalImages {
+    private Integer fiId;
+    private Integer fId;
+    private String fiImages;
+}
--- festgo/src/main/java/com/oplusz/festgo/domain/Festivals.java
+++ festgo/src/main/java/com/oplusz/festgo/domain/Festivals.java
@@ -1,8 +1,33 @@
 package com.oplusz.festgo.domain;
 
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Festivals {
-	// 메롱
+    private Integer fId;
+    private String fName;
+    private LocalDateTime fStartDate;
+    private LocalDateTime fEndDate;
+    private String lcId;
+    private String fAddress;
+    private String fPhone;
+    private String mSponsor;
+    private String fFee;
+    private Integer theId;
+    private String fContents;
+    private String fLat;
+    private String fLong;
+    private String fHomepage;
+    private String fLikes;
+    private String fImageMain;
+    private String fPoster;
+    private LocalDateTime fCreatedTime;
+    private LocalDateTime fModifiedTime;
 }
