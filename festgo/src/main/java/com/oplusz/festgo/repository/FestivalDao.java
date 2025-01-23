a52574ac3ebc5a48294331b321fa7e370d32b6d0
JONGHUKIM <hotsky803@gmail.com>
2025-01-23 12:30:23 +0900

    mapper Update

--- festgo/src/main/java/com/oplusz/festgo/repository/FestivalDao.java
+++ festgo/src/main/java/com/oplusz/festgo/repository/FestivalDao.java
@@ -9,6 +9,6 @@ public interface FestivalDao {
 
 	// 새 축제 작성
 	int insertFestivals(Festival festivals);
-
+	// 축제 이미지들 추가
 	int insertFestivalImagesBatch(List<FestivalImage> festivalImages);
 }
--- festgo/src/main/java/com/oplusz/festgo/web/FestivalController.java
+++ festgo/src/main/java/com/oplusz/festgo/web/FestivalController.java
@@ -1,7 +1,17 @@
 package com.oplusz.festgo.web;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("fest")
 public class FestivalController {
 
 	@GetMapping("/create")
