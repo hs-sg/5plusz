94a3f30b22c12c4dd64f1290ff42ad36ee5cb338
seeunJang <b.lily.ant3@gmail.com>
2025-01-23 18:15:58 +0900

    .

--- festgo/src/main/java/com/oplusz/festgo/repository/PostDao.java
+++ festgo/src/main/java/com/oplusz/festgo/repository/PostDao.java
@@ -6,7 +6,6 @@ import com.oplusz.festgo.domain.Post;
 import com.oplusz.festgo.dto.PostCreateDto;
 
 public interface PostDao {
-
 	List<Post> selectOrderByIdDesc(); // 게시글 목록 조회
 
 	int insert(PostCreateDto dto); // 게시글 생성 쿼리 실행
