a696df1f3b6f916177a8b3caf54305c16de09ed3
hs-sg <hasin927@gmail.com>
2025-02-11 19:55:51 +0900

    alarm 작성중..

--- festgo/src/main/java/com/oplusz/festgo/service/AlarmService.java
+++ festgo/src/main/java/com/oplusz/festgo/service/AlarmService.java
@@ -6,6 +6,8 @@ import java.util.Map;
 import org.springframework.stereotype.Service;
 
 import com.oplusz.festgo.domain.Member;
+import com.oplusz.festgo.dto.AlarmCreateDto;
+import com.oplusz.festgo.repository.AlarmDao;
 import com.oplusz.festgo.repository.FestRequestDao;
 import com.oplusz.festgo.repository.FestivalDao;
 import com.oplusz.festgo.repository.MemberDao;
@@ -18,7 +20,7 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @RequiredArgsConstructor
 public class AlarmService {
-	//private final AlarmDao alarmDao;
+	private final AlarmDao alarmDao;
 	private final MemberDao memberDao;
 	private final SponRequestDao srDao;
 	private final FestivalDao festivalDao;
@@ -39,11 +41,12 @@ public class AlarmService {
 		
 		int meId = memberDao.selectByUsername(meUsername).getMeId();
 		int srId = srDao.selectByMeId(meId).getSrId();
-		int alSfid = 1;
-		//AlarmDto dto = AlarmDto.builder...(meId, srId, alSfid);
-		//return alarmDao.메서드(dto);
+		int alCategory = 1;
+		AlarmCreateDto dto = AlarmCreateDto.builder()
+					.alCategory(alCategory).alSfid(srId).meId(meId)
+					.build();
 		
-		return 0;
+		return alarmDao.insertRequest(dto.toAlarmEntity());
 	}
 	
 	// [사업자용] 축제를 등록할 때 알람 테이블에 데이터 추가
@@ -52,11 +55,25 @@ public class AlarmService {
 		
 		int meId = memberDao.selectByUsername(meUsername).getMeId();
 		int frId = frDao.selectFrIdByFeId(festivalDao.selectFeIdByName(feName));
-		int alSfid = 2;
-		//AlarmDto dto = AlarmDto.builder...(meId, frId, alSfid);
-		//return alarmDao.메서드(dto);
-		
-		return 0;
+		int alCategory = 2;
+		AlarmCreateDto dto = AlarmCreateDto.builder()
+				.alCategory(alCategory).alSfid(frId).meId(meId)
+				.build();
+	
+		return alarmDao.insertRequest(dto.toAlarmEntity());
+	}
+	
+	// [관리자용] 회원가입요청을 승인할 때 알람 테이블에 status 수정
+	public int update(Integer alCategory, Integer sfid) {
+		log.debug("update()");
+		Integer alSfid = sfid;
+		int srId = 0;
+		if (alCategory == 1) {
+			srId = srDao.selectByMeId(sfid).getSrId();
+			alSfid = srId;
+		}
+
+		return alarmDao.updateProcess(alCategory, alSfid);		
 	}
 	
 }
